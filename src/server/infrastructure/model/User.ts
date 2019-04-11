80c3a3ff441112caa01470f5b2600d2abbc1194f
CaptainLucas <lufan1988@gmail.com>
2019-04-10 21:00:16 -0500

    Add type for UserSchema
    1. Add type for UserSchema
    2. Change hash password to be non blocking

--- src/server/infrastructure/model/User.ts
+++ src/server/infrastructure/model/User.ts
@@ -1,26 +1,34 @@
 import * as mongoose from "mongoose";
-import {Model, Schema} from "mongoose"
+import { Document, Model, Schema } from "mongoose";
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    }
+export interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+  date: Date;
+}
+
+const UserSchema = new Schema<User>({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-export const User = mongoose.model('users', UserSchema);
+export const User = mongoose.model<User>("users", UserSchema);
--- src/server/presentation/user-router.ts
+++ src/server/presentation/user-router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import {User} from "../infrastructure/model/User";
+import { User } from "../infrastructure/model/User";
 import * as bcrypt from "bcryptjs";
 import gravatar = require("gravatar");
 
@@ -10,34 +10,39 @@ router.get("/test", (req: Request, res: Response) =>
 );
 
 router.post("/register", (req: Request, res: Response) => {
-    const email: string = req.body.email;
-    User.findOne({email})
-        .then(user => {
+  const email: string = req.body.email;
+  User.findOne({ email }).then(user => {
+    const avatar = gravatar.url(email, {
+      s: "400", //size
+      r: "pg", //rating
+      d: "retro" //default
+    });
 
-            const avatar = gravatar.url(email, {
-                s: '400',   //size
-                r: "pg",    //rating
-                d:"retro"   //default
-            });
-            
-            if(user) {
-                return res.status(400).json({email: `Email ${email} already exists`})
-            } else {
-                const salt = bcrypt.genSaltSync(10)
-                const hash = bcrypt.hashSync(req.body.password, salt);
-                const newUser = new User({
-                    name: req.body.name,
-                    email,
-                    avatar,
-                    password: hash
-                })
-
-                newUser.save().then((user) => {
-                    return res.json(user)
-                }, error => console.log(`Failed to create new User: ${error}`))
-            }
-
+    if (user) {
+      return res.status(400).json({ email: `Email ${email} already exists` });
+    } else {
+      const newUser: User = new User({
+        name: req.body.name,
+        email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt
+        .genSalt(10)
+        .then(salt => {
+          return bcrypt.hash(newUser.password, salt);
         })
-})
+        .then(hashedPassword => {
+          newUser.password = hashedPassword;
+          return newUser.save().then(
+            user => {
+              return res.json(user);
+            },
+            error => console.log(`Failed to create new User: ${error}`)
+          );
+        });
+    }
+  });
+});
 
 export default router;
